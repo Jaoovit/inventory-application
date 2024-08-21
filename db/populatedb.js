c47ed50e20b3a7829e675761bbde8311249b6409
João Oliveira <jaoovit@gmail.com>
2024-08-21 16:44:57 +0100

    define the initial query to database

--- db/populatedb.js
+++ db/populatedb.js
@@ -9,7 +9,20 @@ const DB = process.env.DB;
 const PASSWORD = process.env.PASSWORD;
 const SQLPORT = process.env.SQLPORT;
 
-const SQL = ``;
+const SQL = `
+CREATE TABLE IF NOT EXISTS cars (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    brand VARCHAR(50) NOT NULL,
+    year INT NOT NULL,
+    availability BOOLEAN DEFAULT TRUE,
+     );
+INSERT INTO cars (name, brand, year)
+VALUES
+    ("Corse", "Opel", 2009),
+    ("306", "Peugeot", 2016),
+    ("ix35", "Hyundai", 2009),
+`;
 
 async function main() {
   console.log("Seending the database...");
